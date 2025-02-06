4d04dcc5f37d4963fbf736d59be89ba95766c32a
Salvador Fandiño <sfandino@yahoo.com>
2025-02-07 00:28:46 +0100

    WIP

--- lib/App/SlirpTunnel.pm
+++ lib/App/SlirpTunnel.pm
@@ -1,34 +1,222 @@
 package App::SlirpTunnel;
 
+our $VERSION = '0.01';
+
 use 5.038002;
 use strict;
 use warnings;
 
-require Exporter;
+use POSIX;
+use Socket;
+use IO::Socket::UNIX;
+use File::Spec;
+use App::SlirpTunnel::RPC;
 
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use App::SlirpTunnel ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
+sub hexdump { unpack "H*", $_[0] }
 
 
-# Preloaded methods go here.
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    return $self;
+}
+
+sub start {
+    shift->_start_slave();
+}
+
+sub _my_lib_path {
+    my $my_path = File::Spec->rel2abs($INC{'App/SlirpTunnel.pm'});
+    my @dirs = File::Spec->splitdir($my_path);
+    warn "L->dirs: |$my_path| --> |".join("|", @dirs). "|\n";
+
+    pop @dirs for (1..2);
+        warn "L->dirs: |$my_path| --> |".join("|", @dirs). "|\n";
+    return File::Spec->catdir(@dirs);
+}
+
+sub _start_slave {
+    my $self = shift;
+
+    socketpair(my $parent_socket, my $child_socket, AF_UNIX, SOCK_STREAM, 0)
+        or die "Socket pair creation failed: $!";
+
+    # Step 2: Fork the process
+    my $pid = fork();
+    if (defined $pid && $pid == 0) {
+        # Child process
+        close($parent_socket);
+        POSIX::dup2(fileno($child_socket), 1) or die "dup2 failed: $!";
+        my $lib_path = _my_lib_path();
+        my @sudo_cmd = ("sudo", $^X, "-I".$lib_path, "-MApp::SlirpTunnel::ElevatedSlave", "-e", "App::SlirpTunnel::ElevatedSlave::start");
+        warn "L->Exec: @sudo_cmd\n";
+        # exec { "echo" } ("echo", @sudo_cmd);
+        exec { $sudo_cmd[0] } @sudo_cmd
+            or die "Exec failed: $!";
+    }
+    elsif (not defined $pid) {
+        die "Fork failed: $!";
+    }
+
+    # Parent process
+    close($child_socket);
+    sleep(1);
+
+    $self->{rpc} = App::SlirpTunnel::RPC->new($parent_socket);
+}
+
+sub _request {
+    my ($self, $cmd, %args) = @_;
+    $self->{rpc}->send_packet({cmd => $cmd, args => \%args});
+    my $r = $self->{rpc}->recv_packet();
+    if ($r->{status} eq 'bye') {
+        $self->{rpc} = undef;
+    }
+    return $r
+}
+
+sub hello { shift->_request('hello') }
+
+sub create_tap {
+    my $self = shift;
+    my $r = $self->_request('create_tap');
+    my $device = $r->{device};
+
+    warn "getting device $device";
+    
+    $r->{fd_follows} or die "Failed to setup tap";
+    my $fd = $self->{rpc}->recv_fd();
+
+    # reopen $tap_fd as a Perl file handle
+    my $tap_fh = IO::Socket::UNIX->new_from_fd($fd, "r+")
+        or die "Failed to create tap file handle: $!";
+    return ($device, $tap_fh);
+}
+
+sub device_up {
+    my ($self, $device) = @_;
+    return $self->_request('device_up', device => $device);
+}
+
+sub device_addr_add {
+    my ($self, $device, $addr, $mask) = @_;
+    return $self->_request('device_addr_add', device => $device, addr => $addr, mask => $mask);
+}
+
+sub bye {
+    my $self = shift;
+    return $self->_request('bye');
+}
+
+sub loop {
+    my ($tap_handle, $ssh_handle) = @_;
+
+    my $tap2ssh_buff = '';
+    my $ssh2tap_buff = '';
+    my $pkt_buff;
+    my $max_buff_size = 65*1025;  # 10KB buffer
+
+    my $tap_fd = fileno($tap_handle);
+    my $ssh_fd = fileno($ssh_handle);
+
+    while (1) {
+        my $rfds = '';
+        my $wfds = '';
+        my $efds = $rfds;
+
+        vec($rfds, $ssh_fd, 1) = 1 if length($ssh2tap_buff) < $max_buff_size;
+        vec($rfds, $tap_fd, 1) = 1 if length($tap2ssh_buff) < $max_buff_size;
+
+        my $ssh2tap_pkt_len;
+        if (length($ssh2tap_buff) >= 2) {
+            $ssh2tap_pkt_len = unpack("n", $ssh2tap_buff);
+            if (length($ssh2tap_buff) >= $ssh2tap_pkt_len + 2) {
+                vec($wfds, $tap_fd, 1) = 1;
+            }
+        }
+        if (length($tap2ssh_buff) > 0) {
+            vec($wfds, $ssh_fd, 1) = 1;
+        }
+        # warn "L->tap_fd: $tap_fd, ssh_fd: $ssh_fd\n";
+        #warn "L->Before select, rfds: |".hexdump($rfds)."|, wfds: |".hexdump($wfds)."|\n";
+
+        my $nfound = select($rfds, $wfds, $efds, 1);
+        do {
+            local $!;
+            warn "L->After select, rfds: |".hexdump($rfds)."|, wfds: |".hexdump($wfds)."|, efds: |".hexdump($efds)."| nfound: $nfound, \$!: >$!<\n";
+        };
+
+        if ($nfound <= 0) {
+            warn "L->select failed ($nfound): $!";
+            next;
+        }
+
+        if (vec($rfds, $ssh_fd, 1)) {
+            warn "L->can read from ssh\n";
+            my $n = sysread($ssh_handle, $ssh2tap_buff, $max_buff_size, length($ssh2tap_buff));
+            if (!defined $n) {
+                warn "L->Read from SSH failed: $!";
+            }
+            elsif ($n == 0) {
+                # die "SSH closed connection";
+                warn "SSH closed connection";
+            }
+            else {
+                warn "L->Read packet from ssh, length: $n\n";
+                if (length($ssh2tap_buff) >= 2) {
+                    my $ssh2tap_pkt_len = unpack("n", $ssh2tap_buff);
+                    warn "L->Packet length: $ssh2tap_pkt_len, available: ".length($ssh2tap_buff)."\n";
+                }
+            }
+        }
+        if (vec($rfds, $tap_fd, 1)) {
+            warn "L->can read from tap\n";
+            my $n = sysread($tap_handle, $pkt_buff, $max_buff_size);
+            if (!defined $n) {
+                warn "L->Read from TAP failed: $!";
+            }
+            elsif ($n == 0) {
+                die "TAP closed connection";
+            }
+            else {
+                warn "L->Read packet from tap, length: $n\n";
+                $tap2ssh_buff .= pack("n", $n) . $pkt_buff;
+            }
+        }
+        if (vec($wfds, $ssh_fd, 1)) {
+            warn "L->can write to ssh\n";
+            my $n = syswrite($ssh_handle, $tap2ssh_buff, length($tap2ssh_buff));
+            if (!defined $n) {
+                warn "L->Write to SSH failed: $!";
+            }
+            elsif ($n == 0) {
+                die "SSH closed connection";
+            }
+            else {
+                warn "L->Write to SSH: $n\n";
+                substr($tap2ssh_buff, 0, $n) = '';
+            }
+        }
+        if(vec($wfds, $tap_fd, 1)) {
+            warn "L->can write to tap\n";
+            if (not defined $ssh2tap_pkt_len or length($ssh2tap_buff) < $ssh2tap_pkt_len + 2) {
+                warn "L->Unexpected write flag for TAP";
+            }
+            else {
+                my $n = syswrite($tap_handle, substr($ssh2tap_buff, 2, $ssh2tap_pkt_len));
+                # In any case, we remove the packet from the buffer. The TCP/IP magic!
+                substr($ssh2tap_buff, 0, $ssh2tap_pkt_len + 2) = '';
+                if (!defined $n) {
+                    warn "L->Write to TAP failed: $!";
+                }
+                elsif ($n == 0) {
+                    die "Write to TAP failed: $!";
+                }
+            }
+        }
+    }
+}
 
 1;
 __END__
---
+++ lib/App/SlirpTunnel/ElevatedSlave.pm
@@ -0,0 +1,107 @@
+package App::SlirpTunnel::ElevatedSlave;
+
+use strict;
+use warnings;
+
+use POSIX;
+use Fcntl qw(:mode);
+use Socket;
+use Socket::MsgHdr;
+use IO::Socket::UNIX;
+use App::SlirpTunnel::RPC;
+
+use constant TUNSETIFF => 0x400454ca;  # Define constant for TUNSETIFF
+use constant IFF_TAP   => 0x0002;
+use constant IFF_NO_PI => 0x1000;
+
+sub _create_tap {
+    my $tap_device = "tap0";  # Name of the TAP interface
+
+    sysopen(my $tap_fd, "/dev/net/tun", O_RDWR) or die "Cannot open /dev/net/tun: $!";
+
+    my $ifr = pack("Z16 s", $tap_device, IFF_TAP | IFF_NO_PI);
+    ioctl($tap_fd, TUNSETIFF, $ifr) or die "ioctl TUNSETIFF failed: $!";
+
+    warn "Created TAP device: $tap_device\n";
+    return ($tap_device, $tap_fd);
+}
+
+sub _create_tap__rpc {
+    my $request = shift;
+    my ($tap_device, $tap_fd) = _create_tap();
+    {status => "ok", device => $tap_device, fd => $tap_fd }
+}
+
+sub _do_system {
+    my $self = shift;
+    system(@_) or return { status => "ok" };
+    warn "Command '@_' failed, rc: ".($? >> 8)."\n";
+    return { status => "error" };
+
+}
+
+sub _device_up__rpc {
+    my ($self, $request) = @_;
+    my $device = $request->{device};
+    $self->_do_system("ip", "link", "set", "dev", $device, "up");
+}
+
+sub _device_addr_add__rpc {
+    my ($self, $request) = @_;
+    my $device = $request->{device};
+    my $addr = $request->{addr};
+    my $mask = $request->{mask};
+    $self->_do_system("ip", "addr", "add", "$addr/$mask", "dev", $device);
+}
+
+sub _bye__rpc {
+    return { status => "bye" }
+}
+
+sub _hello__rpc {
+    return { status => "ok" }
+}
+
+sub _run {
+    my $self = shift;
+
+    while (1) {
+        warn "Waiting for request\n";
+        my $request = $self->{rpc}->recv_packet();
+
+        my $cmd = $request->{cmd};
+
+        my $method = "_${cmd}__rpc";
+        my $r = $self->$method($request->{args} // {});
+        my $fd = delete $r->{fd};
+        if (defined $fd) {
+            $r->{fd_follows} = 1;
+        }
+        $self->{rpc}->send_packet($r);
+        if (defined $fd) {
+            $self->{rpc}->send_fd($fd);
+        }
+        last if ($r->{status} eq 'bye')
+    }
+    warn "Bye bye!\n";
+}
+
+sub new {
+    my ($class, $socket) = @_;
+    my $rpc = App::SlirpTunnel::RPC->new($socket);
+    my $self = { socket => $socket,
+                 rpc => $rpc };
+    bless $self, $class;
+}
+
+sub start {
+    POSIX::dup2(1, 3);
+    POSIX::dup2(2, 1);
+    my $socket = IO::Socket::UNIX->new_from_fd(3, "r+")
+        or die "Failed to create socket: $!";
+
+    my $server = App::SlirpTunnel::TapOpener->new($socket);
+    $server->_run();
+}
+
+1;
